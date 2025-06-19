84d52ce2657e1e04e0616943d076223c68ef0baa
Magdalena Dering <magdalena.dering@gigaclear.com>
2025-06-19 20:29:03 +0200

    chore: ESlint & Prettier setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Auth } from "@/components";
+import { Auth } from '@/components'
 
 const Home = async () => {
   return (
     <div className="">
       <Auth />
     </div>
-  );
+  )
 }
 
 export default Home
--- src/components/CapacitorInit.tsx
+++ src/components/CapacitorInit.tsx
@@ -4,9 +4,9 @@ import { Capacitor } from '@capacitor/core'
 import { useEffect } from 'react'
 
 export const CapacitorInit = () => {
-    useEffect(() => {
-        console.log('Capacitor platform:', Capacitor.getPlatform())
-    }, [])
+  useEffect(() => {
+    console.log('Capacitor platform:', Capacitor.getPlatform())
+  }, [])
 
-    return <p>Capacitor Init</p>  
+  return <p>Capacitor Init</p>
 }
